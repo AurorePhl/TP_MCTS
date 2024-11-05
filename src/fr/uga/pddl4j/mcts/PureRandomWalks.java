efec85b876c57f89bf0a2ecd6a69fe01e0196ea9
AurorePhl <asiphilippe@hotmail.com>
2024-11-05 10:43:54 +0100

    update

---
+++ src/fr/uga/pddl4j/mcts/PureRandomWalks.java
@@ -0,0 +1,165 @@
+package fr.uga.pddl4j.mcts;
+
+import fr.uga.pddl4j.heuristics.state.StateHeuristic;
+import fr.uga.pddl4j.parser.DefaultParsedProblem;
+import fr.uga.pddl4j.parser.RequireKey;
+import fr.uga.pddl4j.plan.Plan;
+import fr.uga.pddl4j.plan.SequentialPlan;
+import fr.uga.pddl4j.planners.AbstractPlanner;
+import fr.uga.pddl4j.planners.Planner;
+import fr.uga.pddl4j.planners.PlannerConfiguration;
+import fr.uga.pddl4j.planners.ProblemNotSupportedException;
+import fr.uga.pddl4j.planners.SearchStrategy;
+import fr.uga.pddl4j.planners.statespace.search.StateSpaceSearch;
+import fr.uga.pddl4j.problem.DefaultProblem;
+import fr.uga.pddl4j.problem.Problem;
+import fr.uga.pddl4j.problem.State;
+import fr.uga.pddl4j.problem.operator.Action;
+import fr.uga.pddl4j.problem.operator.ConditionalEffect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import picocli.CommandLine;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * The class implements the Monte-Carlo Tree Search (MCTS) planner with pure random walks in pddl4j. 
+ *
+ * @author Laura DURET & Aurore PHILIPPE
+ * @version 1.0 - 08/11/2024
+ */
+@CommandLine.Command(name = "PureRandomWalks",
+    version = "MCTS 1.0",
+    description = "Solves a specified planning problem using Monte-Carlo tree search strategy with pure random walks.",
+    sortOptions = false,
+    mixinStandardHelpOptions = true,
+    headerHeading = "Usage:%n",
+    synopsisHeading = "%n",
+    descriptionHeading = "%nDescription:%n%n",
+    parameterListHeading = "%nParameters:%n",
+    optionListHeading = "%nOptions:%n")
+public class PureRandomWalks extends AbstractPlanner {
+
+	/**
+     * The class logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(PureRandomWalks.class.getName());
+
+    /**
+     * The weight of the heuristic.
+     */
+    private double heuristicWeight;
+
+    /**
+     * The name of the heuristic used by the planner.
+     */
+    private StateHeuristic.Name heuristic;
+
+    /**
+     * Implement the logic to check if the problem is supported.
+     */
+    @Override
+    public boolean isSupported(Problem problem) {
+        return true; 
+    }
+
+    /**
+     * Sets the weight of the heuristic.
+     *
+     * @param weight the weight of the heuristic. The weight must be greater than 0.
+     * @throws IllegalArgumentException if the weight is strictly less than 0.
+     */
+    @CommandLine.Option(names = {"-w", "--weight"}, defaultValue = "1.0",
+        paramLabel = "<weight>", description = "Set the weight of the heuristic (preset 1.0).")
+    public void setHeuristicWeight(final double weight) {
+        if (weight <= 0) {
+            throw new IllegalArgumentException("Weight <= 0");
+        }
+        this.heuristicWeight = weight;
+    }
+
+    /**
+     * Set the name of heuristic used by the planner to the solve a planning problem.
+     *
+     * @param heuristic the name of the heuristic.
+     */
+    @CommandLine.Option(names = {"-e", "--heuristic"}, defaultValue = "FAST_FORWARD",
+        description = "Set the heuristic : AJUSTED_SUM, AJUSTED_SUM2, AJUSTED_SUM2M, COMBO, "
+            + "MAX, FAST_FORWARD SET_LEVEL, SUM, SUM_MUTEX (preset: FAST_FORWARD)")
+    public void setHeuristic(StateHeuristic.Name heuristic) {
+        this.heuristic = heuristic;
+    }
+
+    /**
+     * Returns the name of the heuristic used by the planner to solve a planning problem.
+     *
+     * @return the name of the heuristic used by the planner to solve a planning problem.
+     */
+    public final StateHeuristic.Name getHeuristic() {
+        return this.heuristic;
+    }
+
+    /**
+     * Returns the weight of the heuristic.
+     *
+     * @return the weight of the heuristic.
+     */
+    public final double getHeuristicWeight() {
+        return this.heuristicWeight;
+    }
+
+
+    /**
+     * Instantiates the planning problem from a parsed problem.
+     *
+     * @param problem the problem to instantiate.
+     * @return the instantiated planning problem or null if the problem cannot be instantiated.
+     */
+    @Override
+    public Problem instantiate(DefaultParsedProblem problem) {
+        final Problem pb = new DefaultProblem(problem);
+        pb.instantiate();
+        return pb;
+    }
+
+    /**
+     * Search a solution plan to a specified domain and problem using Monte-Carlo tree search strategy with pure random walks.
+     *
+     * @param problem the problem to solve.
+     * @return the plan found or null if no plan was found.
+     */
+    @Override
+    public Plan solve(final Problem problem) {
+    	StateSpaceSearch search = StateSpaceSearch.getInstance(SearchStrategy.Name.ASTAR,
+            this.getHeuristic(), this.getHeuristicWeight(), this.getTimeout());
+        LOGGER.info("* Starting MCTS pure random walks \n");
+    	Plan plan = search.searchPlan(problem);
+    	if (plan != null) {
+            LOGGER.info("* MCTS pure random walks succeeded\n");
+            this.getStatistics().setTimeToSearch(search.getSearchingTime());
+            this.getStatistics().setMemoryUsedToSearch(search.getMemoryUsed());
+        } else {
+            LOGGER.info("* MCTS pure random walks failed\n");
+        }
+    	return plan;
+    }
+
+    /**
+     * The main method of the <code>PureRandomWalks</code> planner.
+     *
+     * @param args the arguments of the command line.
+     */
+    public static void main(String[] args) {
+        try {
+            final PureRandomWalks planner = new PureRandomWalks();
+            CommandLine cmd = new CommandLine(planner);
+            cmd.execute(args);
+        } catch (IllegalArgumentException e) {
+            LOGGER.fatal(e.getMessage());
+        }
+    }
+}
