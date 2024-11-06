d0a8a2d9bbd2b7563fdd0d47a57fcddabdad0783
AurorePhl <asiphilippe@hotmail.com>
2024-11-06 15:56:09 +0100

    update

--- src/fr/uga/pddl4j/mcts/PureRandomWalks.java
+++ src/fr/uga/pddl4j/mcts/PureRandomWalks.java
@@ -28,12 +28,6 @@ import java.util.Set;
 
 /**
  * The class implements the Monte-Carlo Tree Search (MCTS) planner with pure random walks in pddl4j.  
- *
- * Features of the Search Algorithm
- * Selection: Choose the node to explore using the UCT heuristic.
- * Expansion: Add new child nodes from the selected node.
- * Simulation: Perform random simulations to estimate reward.
- * Backpropagation: Update rewards and node visits on the return path.
  * 
  * @author Laura DURET & Aurore PHILIPPE
  * @version 1.0 - 08/11/2024
@@ -140,20 +134,162 @@ public class PureRandomWalks extends AbstractPlanner {
      */
     @Override
     public Plan solve(final Problem problem) {
-    	StateSpaceSearch search = StateSpaceSearch.getInstance(SearchStrategy.Name.MCTS,
+    	/*StateSpaceSearch search = StateSpaceSearch.getInstance(SearchStrategy.Name.MCTS,
             this.getHeuristic(), this.getHeuristicWeight(), this.getTimeout());
-        /*StateSpaceSearch search = StateSpaceSearch.getInstance(SearchStrategy.Name.ASTAR,
+        StateSpaceSearch search = StateSpaceSearch.getInstance(SearchStrategy.Name.ASTAR,
             this.getHeuristic(), this.getHeuristicWeight(), this.getTimeout());*/
         LOGGER.info("* Starting MCTS pure random walks \n");
-    	Plan plan = search.searchPlan(problem);
+    	// Plan plan = search.searchPlan(problem);
+        // Search a solution
+        final long begin = System.currentTimeMillis();
+        final Plan plan = this.mcts(problem);
+        final long end = System.currentTimeMillis();
+        // If a plan is found update the statistics of the planner
+        // and log search information
     	if (plan != null) {
             LOGGER.info("* MCTS pure random walks succeeded\n");
-            this.getStatistics().setTimeToSearch(search.getSearchingTime());
-            this.getStatistics().setMemoryUsedToSearch(search.getMemoryUsed());
+            /*this.getStatistics().setTimeToSearch(search.getSearchingTime());
+            this.getStatistics().setMemoryUsedToSearch(search.getMemoryUsed());*/
+            this.getStatistics().setTimeToSearch(end - begin);
         } else {
             LOGGER.info("* MCTS pure random walks failed\n");
         }
+        // Return the plan found or null if the search fails.
     	return plan;
+    }
+
+    /**
+     * Search a solution plan for a planning problem using an Monte-Carlo tree search strategy with pure random walks.
+     * 
+     * Features of the Search Algorithm
+     * Selection: Choose the node to explore using the UCT heuristic.
+     * Expansion: Add new child nodes from the selected node.
+     * Simulation: Perform random simulations to estimate reward.
+     * Backpropagation: Update rewards and node visits on the return path.
+     *
+     * @param problem the problem to solve.
+     * @return a plan solution for the problem or null if there is no solution
+     * @throws ProblemNotSupportedException if the problem to solve is not supported by the planner.
+     */
+    public Plan mcts(Problem problem) throws ProblemNotSupportedException {
+        // Check if the problem is supported by the planner
+        if (!this.isSupported(problem)) {
+            throw new ProblemNotSupportedException("Problem not supported");
+        }
+
+        // First we create an instance of the heuristic to use to guide the search
+        final StateHeuristic heuristic = StateHeuristic.getInstance(this.getHeuristic(), problem);
+
+        // We get the initial state from the planning problem
+        final State init = new State(problem.getInitialState());
+
+        // We initialize the closed list of nodes (store the nodes explored)
+        final Set<Node> close = new HashSet<>();
+
+        // We initialize the opened list to store the pending node according to function f
+        final double weight = this.getHeuristicWeight();
+        final PriorityQueue<Node> open = new PriorityQueue<>(100, new Comparator<Node>() {
+            public int compare(Node n1, Node n2) {
+                double f1 = weight * n1.getHeuristic() + n1.getCost();
+                double f2 = weight * n2.getHeuristic() + n2.getCost();
+                return Double.compare(f1, f2);
+            }
+        });
+
+        // We create the root node of the tree search
+        final Node root = new Node(init, null, -1, 0, heuristic.estimate(init, problem.getGoal()));
+
+        // We add the root to the list of pending nodes
+        open.add(root);
+        Plan plan = null;
+
+        // We set the timeout in ms allocated to the search
+        final int timeout = this.getTimeout() * 1000;
+        long time = 0;
+
+        // We start the search
+        while (!open.isEmpty() && plan == null && time < timeout) {
+
+            // We pop the first node in the pending list open
+            final Node current = open.poll();
+            close.add(current);
+
+            // If the goal is satisfied in the current node then extract the search and return it
+            if (current.satisfy(problem.getGoal())) {
+                return this.extractPlan(current, problem);
+            } else { // Else we try to apply the actions of the problem to the current node
+                for (int i = 0; i < problem.getActions().size(); i++) {
+                    // We get the actions of the problem
+                    Action a = problem.getActions().get(i);
+                    // If the action is applicable in the current node
+                    if (a.isApplicable(current)) {
+                        Node next = new Node(current);
+                        // We apply the effect of the action
+                        final List<ConditionalEffect> effects = a.getConditionalEffects();
+                        for (ConditionalEffect ce : effects) {
+                            if (current.satisfy(ce.getCondition())) {
+                                next.apply(ce.getEffect());
+                            }
+                        }
+                        // We set the new child node information
+                        final double g = current.getCost() + 1;
+                        if (!close.contains(next)) {
+                            next.setCost(g);
+                            next.setParent(current);
+                            next.setAction(i);
+                            next.setHeuristic(heuristic.estimate(next, problem.getGoal()));
+                            open.add(next);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Finally, we return the search computed or null if no search was found
+        return plan;
+    }
+
+    /**
+     * Returns if a specified problem is supported by the planner. Just ADL problem can be solved by this planner.
+     *
+     * @param problem the problem to test.
+     * @return <code>true</code> if the problem is supported <code>false</code> otherwise.
+     */
+    @Override
+    public boolean isSupported(Problem problem) {
+        return (problem.getRequirements().contains(RequireKey.ACTION_COSTS)
+            || problem.getRequirements().contains(RequireKey.CONSTRAINTS)
+            || problem.getRequirements().contains(RequireKey.CONTINOUS_EFFECTS)
+            || problem.getRequirements().contains(RequireKey.DERIVED_PREDICATES)
+            || problem.getRequirements().contains(RequireKey.DURATIVE_ACTIONS)
+            || problem.getRequirements().contains(RequireKey.DURATION_INEQUALITIES)
+            || problem.getRequirements().contains(RequireKey.FLUENTS)
+            || problem.getRequirements().contains(RequireKey.GOAL_UTILITIES)
+            || problem.getRequirements().contains(RequireKey.METHOD_CONSTRAINTS)
+            || problem.getRequirements().contains(RequireKey.NUMERIC_FLUENTS)
+            || problem.getRequirements().contains(RequireKey.OBJECT_FLUENTS)
+            || problem.getRequirements().contains(RequireKey.PREFERENCES)
+            || problem.getRequirements().contains(RequireKey.TIMED_INITIAL_LITERALS)
+            || problem.getRequirements().contains(RequireKey.HIERARCHY))
+            ? false : true;
+    }
+
+    /**
+     * Extracts a search from a specified node.
+     *
+     * @param node    the node.
+     * @param problem the problem.
+     * @return the search extracted from the specified node.
+     */
+    private Plan extractPlan(final Node node, final Problem problem) {
+        Node n = node;
+        final Plan plan = new SequentialPlan();
+        while (n.getAction() != -1) {
+            final Action a = problem.getActions().get(n.getAction());
+            plan.add(0, a);
+            n = n.getParent();
+        }
+        return plan;
     }
 
     /**
